e6d9f2104dbcdffb2bca555a39b583048fdebab3
mikohan <angara99@gmail.com>
2026-03-30 19:29:04 -0700

    Removed page animation

--- app/page.tsx
+++ app/page.tsx
@@ -8,28 +8,28 @@ import PageAnimate from "@/components/PageAnimation"
 
 export default function Page() {
   return (
-    <PageAnimate>
-      <div className="container mx-auto mb-32 max-w-7xl px-8">
-        <div className="mt-2 md:mt-8">
-          <HeroRu />
-        </div>
-        <div className="mt-16">
-          <PerksBar />
-        </div>
-        {/* Section about-start */}
-        <div className="mt-16 md:mt-32">
-          <AboutSectionTwo />
-        </div>
-        <div className="mt-16 md:mt-32">
-          <BenefitsSection />
-        </div>
-        <div className="mt-16 md:mt-32">
-          <WhoINeed />
-        </div>
-        <div className="mt-16 md:mt-32">
-          <CallToAction />
-        </div>
+    // <PageAnimate>
+    <div className="container mx-auto mb-32 max-w-7xl px-8">
+      <div className="mt-2 md:mt-8">
+        <HeroRu />
       </div>
-    </PageAnimate>
+      <div className="mt-16">
+        <PerksBar />
+      </div>
+      {/* Section about-start */}
+      <div className="mt-16 md:mt-32">
+        <AboutSectionTwo />
+      </div>
+      <div className="mt-16 md:mt-32">
+        <BenefitsSection />
+      </div>
+      <div className="mt-16 md:mt-32">
+        <WhoINeed />
+      </div>
+      <div className="mt-16 md:mt-32">
+        <CallToAction />
+      </div>
+    </div>
+    // </PageAnimate>
   )
 }
